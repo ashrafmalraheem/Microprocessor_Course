444b915817c232a918b3d167525556470ed49f57
Ashraf Abdalraheem <ashraf.m.alraheem@gmail.com>
2020-03-28 20:51:44 +0200

    Add Logging python file

---
+++ Lab 3/Logger/Logger.py
@@ -0,0 +1,24 @@
+#******************************************************************************
+# * Copyright (C) 2020 by R&D SEDC
+# *
+# * Redistribution, modification or use of this software in source or binary
+# * forms is permitted as long as the files maintain this copyright. Users are
+# * permitted to modify this and use it to learn about the field of embedded
+# * software. SEDC are not liable for any misuse of this material.
+#****************************************************************************/
+#/**
+# * @file Logger.py
+# * @brief 
+# *
+# *  
+# *
+# * @author: R&D SEDC
+# * @Modifyed: 
+# * @date February 13, 2020, 2:20 PM
+# *
+# */
+
+
+#/*
+# * Section: Included Files
+# */
