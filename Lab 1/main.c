ab03c53e5ac74d67819e52f7001303a99c697c5c
Ashraf Abdalraheem <ashraf.m.alraheem@gmail.com>
2020-04-06 16:35:38 +0200

    Updating main.c

--- Lab 1/main.c
+++ Lab 1/main.c
@@ -24,11 +24,25 @@
  */
 #include <io.h>
 #include <stdint.h>
+#include <delay.h>
+#include "lcd.h"
 
+#define BIT5  0b00100000
 int main(void)
 {
     /* Replace with your application code */
-	uint32_t i;
+	uint32_t i=0;
 	i++;
+	//DDRB |= BIT5;
+	InitLCD(LS_BLINK);
+	LCDClear();
+	LCDWriteString("Hello World");
+	while(0){
+		//PINB = BIT5;
+		//LCDGotoXY(1,1);
+		//LCDWriteString("Hello World");
+		LCDGotoXY(uint8_t x,uint8_t y);
+		_delay_ms(10);
+	}
 }
 
