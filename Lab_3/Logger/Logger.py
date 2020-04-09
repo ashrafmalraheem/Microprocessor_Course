a8a5666c6dd671f8e14f1c18cb3dd4f8f2fed67f
Ashraf Abdalraheem <ashraf.m.alraheem@gmail.com>
2020-04-09 14:05:31 +0200

    The Logging is now working

--- Lab_3/Logger/Logger.py
+++ Lab_3/Logger/Logger.py
@@ -24,16 +24,40 @@
  */'''
 import serial
 import time
-print("Logger Program")
+import os
+import datetime
+## Constants
+BAUD_RATE = 2400
 TX_BUFFER_MAX_SIZE = 100
-ser = serial.Serial('COM13',baudrate=2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None)
-print(ser.name)
+''' Logging Directory and File'''
+DIR = "Logs/"
+if not os.path.exists(DIR):
+    os.makedirs(DIR)
+Log_file_name = DIR + "Log_" + str(datetime.date.today()) + ".log"
+print(Log_file_name)
+log_file = open(Log_file_name,'a+')
+if log_file == None:
+    log_file = open(Log_file_name,'w+')
+print("Logger Program")
+''' Communication port opening '''
+spec_file = open('../specs.mk','r')
+x = 'hello'
+while x != '':
+    x = spec_file.readline()
+    if x.find('COM = ') != -1:
+        COM_PORT = 'COM' + x.strip('COM = \n')
+ser = serial.Serial(COM_PORT,baudrate=BAUD_RATE, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None, exclusive = 1)
 #ser.open(COM13)
 #ser.write(b'hello')
-print(ser.is_open)
-#x = "logger program"
+print(COM_PORT)
 while True :
-	x = ser.read_until(b'\n',size = TX_BUFFER_MAX_SIZE)
-	print(str(x))
-	
+    y = ser.read_until(b'\n',size = TX_BUFFER_MAX_SIZE)
+    x = str(y,'utf-8')
+    x = x.strip('\n')
+    print(COM_PORT,datetime.datetime.now(),x)
+    print(COM_PORT,datetime.datetime.now(),x,file = log_file)
+    #Remove the close when you find a way to exit from Keyboard
+    log_file.close()
+    log_file = open(Log_file_name,'a+')
 ser.close()
+log_file.close()
