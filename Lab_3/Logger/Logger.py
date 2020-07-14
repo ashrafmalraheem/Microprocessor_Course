4c3554fd741c7f58daf80d520a8193a5b88ede1a
Ashraf Abdalraheem <ashraf.m.alraheem@gmail.com>
2020-07-14 22:32:20 +0200

    Use with method instead of opening and closing the log file with each iteration

--- Lab_3/Logger/Logger.py
+++ Lab_3/Logger/Logger.py
@@ -26,6 +26,32 @@ import serial
 import time
 import os
 import datetime
+## Functions
+def logging(log_file):
+    print("Logger Program")
+    ''' Communication port opening '''
+    spec_file = open('../specs.mk','r')
+    x = 'hello'
+    while x != '':
+        x = spec_file.readline()
+        if x.find('COM = ') != -1:
+            COM_PORT = 'COM' + x.strip('COM = \n')
+    ser = serial.Serial(COM_PORT,baudrate=BAUD_RATE, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None, exclusive = 1)
+    #ser.open(COM13)
+    #ser.write(b'hello')
+    print(COM_PORT)
+    while True :
+        y = ser.read_until(b'\n',size = TX_BUFFER_MAX_SIZE)
+        x = str(y,'utf-8')
+        x = x.strip('\n')
+        print(COM_PORT,datetime.datetime.now(),x)
+        print(COM_PORT,datetime.datetime.now(),x,file = log_file)
+        #Remove the close when you find a way to exit from Keyboard
+        #log_file.close()
+        #log_file = open(Log_file_name,'a+')
+    #ser.close()
+
+
 ## Constants
 BAUD_RATE = 2400
 TX_BUFFER_MAX_SIZE = 100
@@ -35,29 +61,10 @@ if not os.path.exists(DIR):
     os.makedirs(DIR)
 Log_file_name = DIR + "Log_" + str(datetime.date.today()) + ".log"
 print(Log_file_name)
-log_file = open(Log_file_name,'a+')
+with open(Log_file_name,'a+') as log_file:
+    logging(log_file)
 if log_file == None:
-    log_file = open(Log_file_name,'w+')
-print("Logger Program")
-''' Communication port opening '''
-spec_file = open('../specs.mk','r')
-x = 'hello'
-while x != '':
-    x = spec_file.readline()
-    if x.find('COM = ') != -1:
-        COM_PORT = 'COM' + x.strip('COM = \n')
-ser = serial.Serial(COM_PORT,baudrate=BAUD_RATE, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None, exclusive = 1)
-#ser.open(COM13)
-#ser.write(b'hello')
-print(COM_PORT)
-while True :
-    y = ser.read_until(b'\n',size = TX_BUFFER_MAX_SIZE)
-    x = str(y,'utf-8')
-    x = x.strip('\n')
-    print(COM_PORT,datetime.datetime.now(),x)
-    print(COM_PORT,datetime.datetime.now(),x,file = log_file)
-    #Remove the close when you find a way to exit from Keyboard
-    log_file.close()
-    log_file = open(Log_file_name,'a+')
-ser.close()
+    with open(Log_file_name,'w+') as log_file:
+        logging(log_file)
+
 log_file.close()
