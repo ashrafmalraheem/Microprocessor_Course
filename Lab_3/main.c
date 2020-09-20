b98af56cffd54f907138495484294bec20f0783e
Ashraf Abdalraheem <ashraf.m.alraheem@gmail.com>
2020-09-20 16:55:24 +0200

    Working main file

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -25,37 +25,28 @@
 #include <io.h>
 #include <stdint.h>
 #include <delay.h>
-#include "lcd.h"
 #include "avr_uart.h"
 #include "LogMessage.h"
 
 #define BIT5  0b00100000
 
 
-#define BAUD_RATE     2400
-#define NO_OF_BITS    _8BITS
+#define BAUD_RATE     19200
+#define NO_OF_BITS    _7BITS
+#define PARITY        EVEN
+#define STOP_BITS     _1BIT
 
 int main(void)
 {
     /* Replace with your application code */
-	//uint32_t i;//=MYUBRR;
 	uint8_t j = 0;
 	float float_k = 33.4;
-	//unsigned char k = 0xA5;
-	//i++;
-	unsigned char Ping_message[11]={0x01, 0x05, 0x0D, 0x41, 0x42, 0x43, 0x44, 0x45, 0xD0, 0xEA,0x04};
-	DDRB |= BIT5;
-	USART_Init(BAUD_RATE, NO_OF_BITS, NONE, 1);
-	_delay_ms(100);
+	USART_Init(BAUD_RATE, NO_OF_BITS, EVEN, 1);
+	_delay_ms(1000);
 	while(1){
 		PINB = BIT5;
-		_delay_ms(1000);
-		//USART_Transmit(j++);
-		Log_Float((char*)"float is now working :) :0",float_k);
-		//float_k *= (j++);
-		/*for(j=0;j<10;j++){
-		USART_Transmit(Ping_message[j]);
-		_delay_ms(1);
-		}*/
+		_delay_ms(100);
+		Log_Float((char*)"float is now working :) :0",float_k+j++);
+		
 	}
 }
